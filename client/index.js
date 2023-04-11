d53a0e476f0ea2ef9344d7c56828c55525881113
sueys <sueys@live.fr>
2023-04-11 14:01:59 +0200

    fix problème sur mon insert dans le tableau

--- client/index.js
+++ client/index.js
@@ -1,80 +1,79 @@
-
-
-document.addEventListener('DOMContentLoaded',()=>{
-    fetch('http://localhost:5000/getAll')
-    .then(response => response.json())
-    .then(data => loadHTMLTable(data['data']));
-    
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTMLTable();
 });
 
 const addBtn = document.querySelector('#add-name-btn');
 
-addBtn.onclick = ()=>{
+addBtn.onclick = () => {
     const nameInput = document.querySelector('#name-input');
     const name = nameInput.value;
     nameInput.value = "";
 
-    fetch('http://localhost:5000/insert',{
-        headers:{
+    fetch('http://localhost:5000/insert', {
+        headers: {
             'Content-type': 'application/json'
         },
-        method:'POST',
-        body: JSON.stringify({ name : name})
+        method: 'POST',
+        body: JSON.stringify({ name: name })
     })
-    .then(response => response.json())
-    .then(data=> insertRowIntoTable(data['data']));
+        .then(response => response.json())
+        .then(data => {
+            loadHTMLTable();
+        });
 }
 
-const insertRowIntoTable=(data)=>{
+
+const insertRowIntoTable = (data) => {
+
     const table = document.querySelector('table tbody');
     const isTableData = table.querySelector('.no-data');
 
     let tableHtml = "<tr>";
 
-    for(const key in data){
-        if(data.hasOwnProperty(key)){
-            if(key ==='dateAdded'){
-                data[key] =new Date(data[key].toDateString());
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            if (key === 'dateAdded') {
+                data[key] = new Date(data[key].toDateString());
             }
-            tableHtml += `<td>${data[key]}</td>`
+            tableHtml += `<td>${data[key]}</td>`;
         }
     }
 
-        tableHtml +=`<td><button class="delete-row-btn" data-id ${data.id}>Delete</td>`;
-        tableHtml +=`<td><button class="edit-row-btn" data-id ${data.id}>Edit</td></td>`;
+    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td></td>`;
 
-
-    tableHtml +=" </tr>"
-
-    if(isTableData){
+    tableHtml += " </tr>"
+    if (isTableData) {
         table.innerHTML = tableHtml;
-    }else{
+    } else {
         const newRow = table.insertRow();
         newRow.innerHTML = tableHtml;
     }
 }
 
-const loadHTMLTable =(data)=>{
-    const table = document.querySelector('table tbody');
+const loadHTMLTable = () => {
+    fetch('http://localhost:5000/getAll')
+        .then(response => response.json())
+        .then(data => {
+            const table = document.querySelector('table tbody');
 
+            if (data['data'].length === 0) {
+                table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+                return;
+            }
 
-    if(data.length ===0){
-        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-        return;
-    }
+            let tableHtml = "";
 
-    let tableHtml ="";
+            data['data'].forEach(({ id, name, date_added }) => {
+                tableHtml += "<tr>";
+                tableHtml += `<td>${id}</td>`;
+                tableHtml += `<td>${name}</td>`;
+                tableHtml += `<td>${new Date(date_added).toLocaleDateString()}</td>`;
+                tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+                tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td></td>`;
+                tableHtml += "</tr>";
+            });
 
-
-    data.forEach(({id,name,date_added})=>{
-        tableHtml +="<tr>";
-        tableHtml +=`<td>${id}</td>`;
-        tableHtml +=`<td>${name}</td>`;
-        tableHtml +=`<td>${new Date(date_added).toLocaleDateString()}</td>`;
-        tableHtml +=`<td><button class="delete-row-btn" data-id ${id}>Delete</td>`;
-        tableHtml +=`<td><button class="edit-row-btn" data-id ${id}>Edit</td></td>`;
-        tableHtml +="</tr>";
-    });
-
-    table.innerHTML =tableHtml;
+            table.innerHTML = tableHtml;
+        });
 }
